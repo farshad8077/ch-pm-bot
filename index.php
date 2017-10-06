1e9b967691ad17ed2550cf4bb85e43dc9de25107
farshad8077 <32514977+farshad8077@users.noreply.github.com>
2017-10-06 06:50:41 +0330

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,78 @@
 include "config.php";
 
 
-define('API_KEY','توکن ربات');
-$admin = "آیدی عددی ادمین";
+define('API_KEY','455923151:AAGJdyp-NWBjdWwf-qaXE_FFcEGvj2V6Zoo');
+$admin = "462092087";
+$channeluse = "-1001086770485";
+
+$update = json_decode(file_get_contents('php://input'));
+$txt = $update->message->text;
+$chat_id = $update->message->chat->id;
+$message_id = $update->message->message_id;
+$channel_forward = $update->channel_post->forward_from;
+$channel_text = $update->channel_post->text;
+$from = $update->message->from->id;
+$chatid = $update->callback_query->message->chat->id;
+$data = $update->callback_query->data;
+$msgid = $update->callback_query->message->message_id;
+
+
+
+function bridge($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query($datas));
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+
+function apiRequest($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query($datas));
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+
+function token($token,$method,$datas=[]){
+    $url = "https://api.telegram.org/bot".$token."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query($datas));
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }<?php
+/**
+ * Created by @OnyxTM.
+ * @CH_PM @CH_PM_BOT
+ * User: Morteza Bagher Telegram id : @mench
+ * Date: 11/12/2016
+ * Time: 09:19 PM
+ */
+
+
+
+include "config.php";
+
+
+define('API_KEY','455923151:AAGJdyp-NWBjdWwf-qaXE_FFcEGvj2V6Zoo');
+$admin = "462092087";
 $channeluse = "-1001086770485";
 
 $update = json_decode(file_get_contents('php://input'));
@@ -69,6 +139,7 @@ function token($token,$method,$datas=[]){
     }else{
         return json_decode($res);
     }
+}
 }
 
 $user = file_get_contents('pmembers.txt');
